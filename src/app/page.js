ab8d0ac589e5e8ed20e72f43429237f5a349007b
Alex Guerra <alexander.a.guerra@gmail.com>
2023-07-06 21:36:46 -0500

    Mass commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,19 @@ export default function Home() {
         <main>
             <h1>Home</h1>
 
-            <Link href="/history">History</Link>
+            <Link href="/history">
+                <p className="p-4">History</p>
+            </Link>
+
+            <p>
+                What is the best way to style a React component? I feel like
+                there might be multiple diffentt ways to try and style them. I
+                could try and style to custome components themselves by adding
+                the className propetry. But, there is something in my bones that
+                says that is not the way to do it. I think If I am going to
+                style the link. Then I need the custome link tag wrapping a
+                native tag that is styled.
+            </p>
 
             <p>
                 I made a link from the homepage to the history page. I should be
